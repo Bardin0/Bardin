0ee6c3ee878428819782f3993bb3b557ab47020d
Michael Giovannini <154377009+Bardin0@users.noreply.github.com.>
2026-05-23 18:36:28 -0400

    Added user auth files

---
+++ app/backend/src/main/java/com/bardin/backend/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.bardin.backend.auth.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.bardin.backend.auth.User;
+
+import com.bardin.backend.auth.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"
+                        )
+                );
+        return new CustomUserDetails(user);
+    }
+}
